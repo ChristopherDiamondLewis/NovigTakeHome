1d5d50b7962c2299529f9343e34dad47f356b07a
Chris Lew <clew1996@gmail.com>
2026-03-02 22:24:04 -0500

    chore: added leader and replica start scripts for their respective containers

---
+++ start_calculator/leader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting leader service.."
+exec ../build/shared_calculator/shared_calculator_leader/shared_calculator_leader
---
+++ start_calculator/replica.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting replica service..."
+exec ../build/shared_calculator/shared_calculator_replica/shared_calculator_replica
